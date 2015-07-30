e6c0b08587083d86da897f1ea212710be763dd7b
Tyler Machen <tylermachen@gmail.com>
2015-07-30 13:54:54 -0400

    fix levitation quiz by removing method call

--- levitation_quiz.rb
+++ levitation_quiz.rb
@@ -1,11 +1,8 @@
-
 def levitation_quiz
-	loop do
-    puts "What is the spell that enacts levitation?"  
-    answer = gets.chomp
-    break if answer == "Wingardium Leviosa"
-  end
-  puts "You passed the quiz!"
+    loop do 
+        puts "What is the spell that enacts levitation?"
+        answer = gets.chomp
+        break if answer == "Wingardium Leviosa"
+    end
+    puts "You passed the quiz!"
 end
-
-levitation_quiz
